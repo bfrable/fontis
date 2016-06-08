dce698d95e3115e5dfe7e4eb0fb5e93a36739bc2
Brett <bfrable@redventures.com>
2016-06-08 19:34:23 -0400

    added responsive nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,37 @@
     'use strict';
     
     var helpers = {
+        mobile_nav: {
+            toggle: function() {
+                var els = {
+                    open:  $('.js-toggle-open'),
+                    close: $('.js-toggle-close')
+                };
+
+                els.open.on('click', function() {
+                    $(this).addClass('nav-toggle--hidden');
+                    els.close.removeClass('nav-toggle--hidden');
+                    helpers.mobile_nav.toggle_nav();
+                });
+
+                els.close.on('click', function() {
+                    $(this).addClass('nav-toggle--hidden');
+                    els.open.removeClass('nav-toggle--hidden');
+                    helpers.mobile_nav.toggle_nav();
+                });
+            },
+            toggle_nav: function() {
+                var nav = $('.js-mobile-nav');
+
+                nav.toggleClass('js-nav-hidden');
+            },
+            init: function() {
+                helpers.mobile_nav.toggle();
+            }
+        },
+        
         init: function() {
-            console.log('hello');
+            helpers.mobile_nav.init();
         }
     };
 
